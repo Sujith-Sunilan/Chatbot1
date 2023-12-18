e693afcd9f8408abbc472584b3eec744afbf2bfe
Sujith-Sunilan <53692963+Sujith-Sunilan@users.noreply.github.com>
2023-12-18 16:35:18 +0530

    chatbot2

---
+++ static/script/responses.js
@@ -0,0 +1,15 @@
+function getBotResponse(input){
+
+    if(input =="rock"){
+        return "paper";
+    }
+    if(input=="hello"){
+        return "Hello there!";
+    }else if(input=="goodbaye")
+    {
+        return "talk to you later!";
+    }
+    else{
+        return "Try asking something else!";
+    }
+}
